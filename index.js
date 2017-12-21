f89bf568e323d223b79a8e07a2f88ae805360b07
Jonathon Ball <jonathon.ball@gmail.com>
2017-12-21 00:31:42 -0500

    created event for when youtube info requests return

--- index.js
+++ index.js
@@ -2,22 +2,26 @@
 process.name = 'canticle';
 
 const Canticle = require('./lib/canticle');
+const YoutubeInfoGather = require('./lib/youtube-canticle');
 const config = require('config');
 
 const canticle = new Canticle();
-var playlists = config.get('playlists');
+const youtube = new YoutubeInfoGather();
 
+var playlists = config.get('playlists');
 
 canticle.on('playlistManagerConsole', (userInput) => {
     switch (userInput.cmd) {
         case 'add':
+        case 'insert':
+        case 'create':
+        case '+':
             canticle.screen.log('user adding');
             canticle.screen.render();
             break;
         default:
             canticle.playlistManagerLog.log(userInput.cmd + " command unknown");
     }
-
 });
 /*
 const blessed = require('./lib/blessed-canticle');
