396571245b7a832f66b75f5ca4ccc5ad0ae00e71
Jonathon Ball <jonathon.ball@gmail.com>
2017-12-22 00:13:59 -0500

    playlist manager now uses command translator

--- index.js
+++ index.js
@@ -3,23 +3,25 @@ process.name = 'canticle';
 
 const Canticle = require('./lib/canticle');
 const YoutubeInfoGather = require('./lib/youtube-canticle');
+const CommandTranslator = require('./lib/command-translation');
 const config = require('config');
 
 const canticle = new Canticle();
 const youtube = new YoutubeInfoGather();
+const translate = new CommandTranslator();
 
 var playlists = config.get('playlists');
 
 canticle.on('playlistManagerConsole', (userInput) => {
-    switch (userInput.cmd) {
+    let translatedCmd = translate.findCommand(userInput.cmd);
+    switch (translatedCmd) {
         case 'add':
-        case 'insert':
-        case 'create':
-        case '+':
             canticle.screen.log('user adding');
             // TODO validate params
             canticle.playlistManagerList.addItem(userInput.params);
             canticle.screen.render();
+            break;
+        case 'delete':
             break;
         default:
             canticle.playlistManagerLog.log(userInput.cmd + " command unknown");
