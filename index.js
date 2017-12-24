6383b0aed45a1305224d1825893f284bc0b0c3e9
Jonathon Ball <jonathon.ball@gmail.com>
2017-12-23 23:16:37 -0500

    playlists can now be added

--- index.js
+++ index.js
@@ -3,25 +3,33 @@ process.name = 'canticle';
 
 const YoutubeInfoGather = require('./lib/youtube-canticle');
 const CommandTranslator = require('./lib/command-translation');
-const Storage = require('./lib/storage');
-const storage = new Storage();
-const Canticle = require('./lib/canticle');
-const canticle = new Canticle();
 const youtube = new YoutubeInfoGather();
 const translate = new CommandTranslator();
 
+const Storage = require('./lib/storage');
+const storage = new Storage();
 
+const Canticle = require('./lib/canticle');
+const canticle = new Canticle();
 
 canticle.on('playlistManagerConsole', (userInput) => {
     let translatedCmd = translate.findCommand(userInput.cmd);
     switch (translatedCmd) {
         case 'add':
-            canticle.playlistManagerAddItem(userInput.params);
+            storage.addPlaylist(userInput.params);
             break;
         case 'delete':
             canticle.playlistManagerRemoveItem(userInput.params);
             break;
         default:
             canticle.plmLog.log(userInput.cmd + " command unknown");
+    }
+});
+
+storage.on('playlist_add', (playlistName) => {
+    if (playlistName.hasOwnProperty('failure')) {
+        canticle.plmLog.log('error adding ' + playlistName.playlistName);
+    } else {
+        canticle.playlistManagerAddItem(playlistName);
     }
 });
