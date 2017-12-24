4837ba447fb7454beb6fd4acbe40e90440c9b16c
Jonathon Ball <jonathon.ball@gmail.com>
2017-12-23 20:47:42 -0500

    added sequelize ORM

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 process.name = 'canticle';
 
-const Canticle = require('./lib/canticle');
 const YoutubeInfoGather = require('./lib/youtube-canticle');
 const CommandTranslator = require('./lib/command-translation');
-const config = require('config');
-
+const Storage = require('./lib/storage');
+const storage = new Storage();
+const Canticle = require('./lib/canticle');
 const canticle = new Canticle();
 const youtube = new YoutubeInfoGather();
 const translate = new CommandTranslator();
 
-var playlists = config.get('playlists');
+
 
 canticle.on('playlistManagerConsole', (userInput) => {
     let translatedCmd = translate.findCommand(userInput.cmd);
