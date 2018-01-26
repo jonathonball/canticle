0aae0f263a307ce28c8a84e5214a1e9306cb589b
Jon Ball <jonathon.ball@gmail.com>
2018-01-26 10:46:15 -0500

    added command translator

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const UserInterface = require('./lib/user_interface');
+const CommandTranslator = require('./lib/command_translator');
 
 var userInterface = new UserInterface();
-
+var commandTranslator = new CommandTranslator();
 
 userInterface.on('shutdown', () => {
     console.log('Goodbye!');
-})
+});
