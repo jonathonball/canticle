16cf3ca48d2340077dba5c394c37e88a2f084908
Jonathon Ball <jonathon.ball@gmail.com>
2018-01-28 22:41:39 -0500

    added initial playlist loading as ORM test

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const Storage = require('./lib/storage');
 
 var storage = new Storage();
 
-storage.on('ready', () => {
+storage.on('ready', (playlists) => {
     var userInterface = new UserInterface();
+
+    userInterface.playlistManager.addPlaylists(playlists);
 
     userInterface.on('shutdown', () => {
         console.log('Goodbye!');
