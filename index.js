5585c7dd65fd1b90fe09de76c6dd082852c2f637
Jon Ball <jonathon.ball@gmail.com>
2018-01-31 14:42:06 -0500

    added playlist playback and better cleanup when app crashes

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ var storage = new Storage();
 storage.on('ready', (initialPlaylists) => {
     var userInterface = new UserInterface(storage);
 
-    userInterface.on('quit', () => {
+    userInterface.on('shutdown', () => {
         console.log('Goodbye!');
+    });
+
+    process.on(['exit', 'SIGINT', 'SIGUSR1', 'SIGUSR2', 'uncaughtException'], () => {
+        userInterface.shutdown();
     });
 
     userInterface.playlistManager.addPlaylists(initialPlaylists);
