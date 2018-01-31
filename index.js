4e4757b67eb1f25df7e1e4e10c104b73ded64dde
Jon Ball <jonathon.ball@gmail.com>
2018-01-31 13:24:12 -0500

    moved userInterface events into that class

--- index.js
+++ index.js
@@ -1,76 +1,14 @@
 const UserInterface = require('./lib/user_interface');
 const Storage = require('./lib/storage');
-const ValidateUrl = require('valid-url');
-const YouTubeDl = require('youtube-dl');
 
 var storage = new Storage();
 
 storage.on('ready', (initialPlaylists) => {
-    var userInterface = new UserInterface();
+    var userInterface = new UserInterface(storage);
 
-    userInterface.on('command_add_playlist', (name) => {
-        let addPlaylistQuery = storage.Playlist.create({ name: name });
-        userInterface.playlistManager.addPlaylist(addPlaylistQuery);
-    });
-
-    userInterface.on('command_add_track', (uri) => {
-        if (userInterface.playlist.isLoaded()) {
-            userInterface.log.log('Loading track info.');
-            if (ValidateUrl.isWebUri(uri)) {
-                 YouTubeDl.getInfo(uri, (err, info) => {
-                     userInterface.log.log(info.url);
-                     let addTrackQuery = storage.Track.create({
-                         title: info.title,
-                         url: uri,
-                         playlist_id: userInterface.playlist.playlist.id,
-                     });
-                     userInterface.playlist.addTrack(addTrackQuery);
-                 });
-             } else {
-                 userInterface.log.log('Track must be a web uri');
-             }
-        } else {
-            userInterface.log.log('No playlist loaded.');
-        }
-    });
-
-    userInterface.on('command_delete_playlist', (name) => {
-        let deletePlaylistQuery = storage.Playlist.destroy({ where: { name: name } });
-        userInterface.playlistManager.deletePlaylist(deletePlaylistQuery, name);
-    });
-
-    userInterface.on('command_open_playlist', (name) => {
-        let playlist = userInterface.playlistManager.getPlaylist(name);
-        if (playlist) {
-            userInterface.playlistManager.select(playlist.name);
-            userInterface.playlist.addTracks(playlist);
-        } else {
-            userInterface.log.log('Error loading playlist ' + name);
-        }
-    });
-
-    userInterface.on('command_open', () => {
-        let playlist = userInterface.playlist.getSelected();
-        if (userInterface.playlist.isLoaded() && playlist) {
-            userInterface.log.log('Loading ' + playlist.title);
-            YouTubeDl.getInfo(playlist.url, (err, info) => {
-                let audioStreams = info.formats.filter(({vcodec}) => vcodec == 'none');
-                userInterface.player.duration = info.duration;
-                userInterface.player.open(audioStreams[0].url);
-                userInterface.screen.render();
-            });
-        } else {
-            userInterface.log.log('No playlist is loaded.');
-        }
-    });
-
-    userInterface.on('command_close', () => {
-        userInterface.screen.destroy();
-        userInterface.player.quit();
+    userInterface.on('quit', () => {
         console.log('Goodbye!');
     });
 
-    userInterface.log.log('Welcome to Canticle.');
     userInterface.playlistManager.addPlaylists(initialPlaylists);
-    userInterface.commandConsole.focus();
 });
